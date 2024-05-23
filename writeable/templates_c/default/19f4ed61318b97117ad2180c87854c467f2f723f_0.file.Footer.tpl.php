dfda4059ba8f432c57058c9fa4c77db513650979
mesvaktrums <aria.sh@techtrums.com.au>
2024-05-23 11:40:56 +1000

    base version
    this is the base version

---
+++ writeable/templates_c/default/19f4ed61318b97117ad2180c87854c467f2f723f_0.file.Footer.tpl.php
@@ -0,0 +1,29 @@
+<?php
+/* Smarty version 3.1.39, created on 2024-04-29 13:53:29
+  from '/home/meromedi/portal.meromedika/layouts/default/templates/Footer.tpl' */
+
+/* @var Smarty_Internal_Template $_smarty_tpl */
+if ($_smarty_tpl->_decodeProperties($_smarty_tpl, array (
+  'version' => '3.1.39',
+  'unifunc' => 'content_662f19b92cfdb5_42665396',
+  'has_nocache_code' => false,
+  'file_dependency' => 
+  array (
+    '19f4ed61318b97117ad2180c87854c467f2f723f' => 
+    array (
+      0 => '/home/meromedi/portal.meromedika/layouts/default/templates/Footer.tpl',
+      1 => 1714362561,
+      2 => 'file',
+    ),
+  ),
+  'includes' => 
+  array (
+  ),
+),false)) {
+function content_662f19b92cfdb5_42665396 (Smarty_Internal_Template $_smarty_tpl) {
+?>
+</div>
+</body>
+</html>
+<?php }
+}
